fef7c5f4c024f7977732447ef6808f42a06c1569
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-04-29 12:56:55 -0700

    pass seventh Definition test: returns null when no Definition found.

--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -44,10 +44,10 @@ public class DefinitionTest {
     assertEquals(Definition.find(secondDefinition.getId()), secondDefinition);
   }
 
-  // @Test
-  // public void find_returnsNullWhenNoDefinitionFound_null() {
-  //   assertTrue(Definition.find(999) == null);
-  // }
+  @Test
+  public void find_returnsNullWhenNoDefinitionFound_null() {
+    assertTrue(Definition.find(999) == null);
+  }
 
 
 
